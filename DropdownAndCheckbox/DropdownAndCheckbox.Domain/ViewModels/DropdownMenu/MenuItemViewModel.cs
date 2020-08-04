23e6e15dad3a522bec27a382af0cc5c1ff59d421
Евгений <vasilev.jv@gmail.com>
2020-08-04 23:37:19 +0700

    Добавлен ViewComponent для построения меню

---
+++ DropdownAndCheckbox/DropdownAndCheckbox.Domain/ViewModels/DropdownMenu/MenuItemViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DropdownAndCheckbox.Domain.ViewModels.DropdownMenu
+{
+    /// <summary>
+    /// Элемент списка меню
+    /// </summary>
+    public class MenuItemViewModel
+    {
+        /// <summary>
+        /// Иконка ссылки
+        /// </summary>
+        public string ImageIcon { get; set; }
+
+        /// <summary>
+        /// Отображаемое наименование ссылки меню
+        /// </summary>
+        public string NameLink { get; set; }
+
+        /// <summary>
+        /// Адрес ссылки меню
+        /// </summary>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Список дочерних ссылок меню
+        /// </summary>
+        public IEnumerable<MenuItemViewModel> ChilrdenMenuItems { get; set; }
+
+        /// <summary>
+        /// Элемент меню - разделитель
+        /// </summary>
+        public bool IsDivider { get; set; } = false;
+
+        /// <summary>
+        /// Пустой элемент меню
+        /// </summary>
+        public bool IsEmpty { get; set; } = false;
+
+
+    }
+}
